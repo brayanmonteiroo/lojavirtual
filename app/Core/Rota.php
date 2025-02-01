cdd3ed2631e2dc7460bdf66eda142a1b3ac5f7f8
Brayan Monteiro <brayanmonteirooo@gmail.com>
2025-02-01 11:20:19 -0300

    Introdução a Classe de Rotas

---
+++ app/Core/Rota.php
@@ -0,0 +1,11 @@
+<?php 
+
+namespace app\Core;
+
+class Rota
+{
+    public function __construct()
+    {
+        echo 'Classe Rota!';
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,3 +1,9 @@
 <?php 
-echo 'Hello, world!';
-?>
+
+include_once '../app/Core/Rota.php';
+
+use app\Core\Rota;
+
+$rota = new Rota();
+
+var_dump($rota);
