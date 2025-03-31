66be5f03f04719f711faf5b08700cd68dbf006be
Brayan Monteiro <brayanmonteirooo@gmail.com>
2025-03-31 20:38:21 -0300

    Criação das views

---
+++ app/Views/site/base.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Titulo</title>
+</head>
+<body>
+    <?php include 'topo.php'?>
+
+    <?= $conteudo ?? ''; ?>
+
+    <?php include 'rodape.php'?>
+
+</body>
+</html>
---
+++ app/Views/site/topo.php
@@ -0,0 +1,3 @@
+<header>
+    <h1>CABEÇALHO</h1>
+</header>
